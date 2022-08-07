1ce9790fba4b105d748479e175fb129d475a879a
Jason Qin <jiasheng.qin@student.unsw.edu.au>
2022-08-07 16:16:00 +1000

    Update parser

--- src/parser.rs
+++ src/parser.rs
@@ -2,3 +2,8 @@
 // Maybe build.rs can run this function to expand 
 
 pub fn parse_axml() {}
+
+// parse tags
+// any embedded rust {} blocks gets parsed by the macro engine that parses those specific blocks
+// each element becomes a struct with a render view
+// a render view is a partial spec that can be executed by the cpu or gpu compute unit to create the command buffer to actually render that somewhere
