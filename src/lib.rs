8d38608983b6dcde3c5ef0a4ec30c3045f4a036d
Jason Qin <jiasheng.qin@student.unsw.edu.au>
2022-08-07 11:41:19 +1000

    Update lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,13 @@
+/// Bundle your arcen code (rust crate) into a native Neutron Arc App
+pub mod bundle;
 pub mod component;
 /// Parser for ArcXML (AXML), a yew functional like syntax for describing a webpage
 pub mod parser;
+/// Useful components and functions often used, and per os
+pub mod prelude;
 pub mod renderer;
 /// Lightweight type to pass around and into wgpu
 pub mod types;
-/// Bundle your arcen code (rust crate) into a native Neutron Arc App
-pub mod bundle;
+pub use prelude::*;
 
 pub fn start_arcen() {}
-
