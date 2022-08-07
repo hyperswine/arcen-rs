db3436ce3af71c3765f1a96ef2a4b0e1546c1645
Jason Qin <jiasheng.qin@student.unsw.edu.au>
2022-08-07 10:45:00 +1000

    Add default component skels

--- src/component/mod.rs
+++ src/component/mod.rs
@@ -1,2 +1,18 @@
 // In arcen, the default container is a flexbox
 // You can use grid and stuff, but that can be annoying, <Flex/> just works
+
+// A box is a static node with default properties
+
+// maybe make them (Node) with specific properties
+
+use crate::renderer::node::Node;
+
+pub struct Box(Node);
+
+pub struct Flex(Node);
+
+pub struct Heading(Node);
+
+pub struct Text(Node);
+
+pub struct Image(Node);
