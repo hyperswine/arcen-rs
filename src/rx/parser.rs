34c8e4259e8c787305d8c3ec6869f87816bcabea
Jason Qin <jiasheng.qin@student.unsw.edu.au>
2022-08-14 01:27:47 +1000

    Update parsing!!

--- src/rx/parser.rs
+++ src/rx/parser.rs
@@ -4,7 +4,9 @@ use log::debug;
 use logos::Logos;
 use std::ops::Range;
 
-use super::expr::ElementDef;
+use crate::rust_expr;
+
+use super::expr::{ElementDef, Expr, Literal, ScopeExpr};
 
 #[derive(Logos, Debug, PartialEq, Clone, Copy)]
 pub enum Token {
@@ -101,6 +103,17 @@ impl Parser {
         Err(t.0)
     }
 
+    pub fn accept_index(&mut self, token: Token) -> Result<usize, Token> {
+        let t = &self.tokens[self.curr_index];
+
+        if token == t.0 {
+            // get the index of t
+            return Ok(self.curr_index);
+        }
+
+        Err(t.0)
+    }
+
     /// Simply returns true or false
     pub fn accept_ok(&mut self, token: Token) -> bool {
         self.accept(token).is_ok()
@@ -113,6 +126,19 @@ impl Parser {
                 panic!("Token \"{token:?}\" was not expected... \"{e:?}\" was the actual token")
             }
         }
+    }
+
+    pub fn expect_index(&mut self, token: Token) -> usize {
+        match self.accept_index(token) {
+            Ok(t) => t,
+            Err(e) => {
+                panic!("Token \"{token:?}\" was not expected... \"{e:?}\" was the actual token")
+            }
+        }
+    }
+
+    pub fn substr(&self, range_start: usize, range_end: usize) -> String {
+        self.input_string[range_start..range_end].to_owned()
     }
 
     pub fn log_tokens(&self) {
@@ -144,13 +170,81 @@ pub fn rx(parser: &mut Parser) -> ExprRes<ElementDef> {
     // check for "rx"
     parser.accept(Token::Rx)?;
 
+    // expect a scope expression
+    parser.expect(Token::CurlyBracketLeft);
+
+    // take what ever is in the scope, a list of child elements
+
+    parser.expect(Token::CurlyBracketRight);
+
     expr_none!()
 }
 
+pub fn element_expr(parser: &mut Parser) -> ExprRes<ElementDef> {
+    let ident = parser.accept(Token::Identifier)?;
+
+    // if any params, take em
+    if parser.accept_ok(Token::ParenthesisLeft) {
+        while let Ok(arg_ident) = parser.accept(Token::Identifier) {
+            // let val be the answer of the expression
+            // an arg's value should match its type. Which is usually a rust like type, either a single var, tuple or array
+            // it can be an rx expression, which is pretty much a literal, identifier or string
+            // or it can be a rust expression, which would highlighted by braces {}
+
+            // e.g. Box(height="2rem"), Box(h=2) are valid forms
+            // Box(height={var}) means you want to use a rust variable
+
+            // expect
+            parser.expect(Token::Equals);
+
+            if let Ok(scope) = scope_expr(parser) {
+                // interpret the expression body as a string
+            } else {
+                // an rx expression or prob just literal/ident
+                let val = literal(parser).unwrap();
+            }
+        }
+
+        parser.expect(Token::ParenthesisRight);
+    }
+
+    expr_none!()
+}
+
+/// Looks for a generic expression like those found in arg values (positional) and {rust_scope_expr} for injecting rust code into the view. E.g. map(|f| Box{})
+pub fn expr(parser: &mut Parser) -> ExprRes<Expr> {
+    expr_none!()
+}
+
+pub fn scope_expr(parser: &mut Parser) -> ExprRes<ScopeExpr> {
+    let start = parser.accept_index(Token::CurlyBracketLeft)?;
+    // expect a rust expr, although we could just expect whitespace and not deal with it
+    // uhh, idk is it possible to invoke syn here for the next token?
+    // we want either an identifier or a path or an expression
+
+    // need to either get next symbol and its span (string) or simply try to "parse" the stuff as a scoped expr and pass it to rust_expr!
+
+    // check for an expression, and also get the span of the left curly to right curly
+    // how?. Maybe take note of index of left, then take note of right. Then ask parser for the substr
+
+    // rust_expr!();
+
+    let expr = expr(parser).unwrap();
+
+    let end = parser.expect_index(Token::CurlyBracketRight);
+
+    let string = parser.substr(start, end);
+
+    Ok(ScopeExpr { expr, string })
+}
+
 // a literal should be either a double quoted string or int
-pub fn literal(parser: &mut Parser) {}
+pub fn literal(parser: &mut Parser) -> ExprRes<Literal> {
+    expr_none!()
+}
 
 /// Parse tokens (e.g. an rx input string) into a rust string
+/// First converts the rx found into AST's. Then converts each AST into their rust forms and replaces their original spans with the rust
 pub fn parse_to_rust(parser: &mut Parser) -> String {
     // maybe a parse impl instead? for syn
     // also proc_macro2 somewhere
@@ -160,3 +254,6 @@ pub fn parse_to_rust(parser: &mut Parser) -> String {
     // otherwise, prob just rust. Hand off to rust parser
     String::from("")
 }
+
+// We want to recursively look for instances of RX in the input string
+// There may be a rust part that contains an rx part, which contains a rust part, which contains an rx part...
